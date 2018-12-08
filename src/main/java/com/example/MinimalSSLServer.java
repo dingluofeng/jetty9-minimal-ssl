31b630b85b55cbc58b4c575a0a88369f10b60344
riversun <riversun.org@gmail.com>
2018-12-08 13:46:48 +0900

    Add package

--- src/main/java/com/example/MinimalSSLServer.java
+++ src/main/java/com/example/MinimalSSLServer.java
@@ -1,24 +1,17 @@
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
+package com.example;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
-
 public class MinimalSSLServer {
 	public static void main(String[] args) throws Exception {
-
 		final Server server = new Server();
 		final SslContextFactory sslContextFactory = new SslContextFactory();
-
 		sslContextFactory.setKeyStorePath(System.getProperty("user.dir") + "/mykeystore.jks");
 		sslContextFactory.setKeyStorePassword("mypassword");
-
 		final ServerConnector httpsConnector = new ServerConnector(server, sslContextFactory);
 		httpsConnector.setPort(443);
-
 		final ResourceHandler resourceHandler = new ResourceHandler();
 		resourceHandler.setResourceBase(System.getProperty("user.dir") + "/htdocs");
-
 		server.setConnectors(new Connector[] { httpsConnector });
 		server.setHandler(resourceHandler);
 		server.start();
