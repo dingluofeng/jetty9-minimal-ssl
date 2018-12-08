8c81ed7d8c71d6240d104303cbfea099b9a36fdd
riversun <riversun.org@gmail.com>
2018-12-08 15:28:01 +0900

    Add comments Modify pom Add example keystore

--- src/main/java/com/example/MinimalSSLServer.java
+++ src/main/java/com/example/MinimalSSLServer.java
@@ -1,20 +1,47 @@
 package com.example;
+
 import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+/**
+ * Minimal SSL Server on Jetty9
+ * 
+ * @author Tom Misawa (riversun.org@gmail.com)
+ *
+ */
 public class MinimalSSLServer {
-	public static void main(String[] args) throws Exception {
-		final Server server = new Server();
-		final SslContextFactory sslContextFactory = new SslContextFactory();
-		sslContextFactory.setKeyStorePath(System.getProperty("user.dir") + "/mykeystore.jks");
-		sslContextFactory.setKeyStorePassword("mypassword");
-		final ServerConnector httpsConnector = new ServerConnector(server, sslContextFactory);
-		httpsConnector.setPort(443);
-		final ResourceHandler resourceHandler = new ResourceHandler();
-		resourceHandler.setResourceBase(System.getProperty("user.dir") + "/htdocs");
-		server.setConnectors(new Connector[] { httpsConnector });
-		server.setHandler(resourceHandler);
-		server.start();
-		server.join();
-	}
+
+    public static void main(String[] args) throws Exception {
+
+        // create jetty server
+        final Server server = new Server();
+
+        // create factory for ssl
+        final SslContextFactory sslContextFactory = new SslContextFactory();
+
+        // Set keystore file path
+        sslContextFactory.setKeyStorePath(System.getProperty("user.dir") + "/mykeystore.jks");
+
+        // Set keystorepassword
+        sslContextFactory.setKeyStorePassword("mypassword");
+
+        // create connector for https
+        final ServerConnector httpsConnector = new ServerConnector(server, sslContextFactory);
+        httpsConnector.setPort(443);
+
+        // create ResourceHandler for static contents
+        final ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase(System.getProperty("user.dir") + "/htdocs");
+
+        // Set connector
+        server.setConnectors(new Connector[] { httpsConnector });
+
+        // Set handler
+        server.setHandler(resourceHandler);
+
+        // Start server
+        server.start();
+        server.join();
+    }
 }
